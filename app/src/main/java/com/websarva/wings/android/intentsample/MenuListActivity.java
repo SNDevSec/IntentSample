aaff92a1b77e11d2d1a30a8fe031135140355e2a
Hirokazu Kamachi <h.kamachi0507@gmail.com>
2018-12-23 23:56:25 +0900

    2

--- app/src/main/java/com/websarva/wings/android/intentsample/MenuListActivity.java
+++ app/src/main/java/com/websarva/wings/android/intentsample/MenuListActivity.java
@@ -1,7 +1,17 @@
 package com.websarva.wings.android.intentsample;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MenuListActivity extends AppCompatActivity {
 
@@ -9,5 +19,45 @@ public class MenuListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_list);
+
+        ListView lvMenu = findViewById(R.id.lvMenu);
+        List<Map<String, String>> menuList = new ArrayList<>();
+        Map<String, String> menu = new HashMap<>();
+        menu.put("name", "から揚げ定");
+        menu.put("price", "800円");
+        menuList.add(menu);
+
+        menu = new HashMap<>();
+        menu.put("name", "ハンバーグ定");
+        menu.put("price", "1000円");
+        menuList.add(menu);
+
+        menu = new HashMap<>();
+        menu.put("name", "焼き魚定");
+        menu.put("price", "900円");
+        menuList.add(menu);
+
+        String[] from = {"name", "price"};
+        int[] to = {android.R.id.text1, android.R.id.text2};
+        SimpleAdapter adapter = new SimpleAdapter(MenuListActivity.this, menuList, android.R.layout.simple_list_item_2, from, to);
+        lvMenu.setAdapter(adapter);
+
+        lvMenu.setOnItemClickListener(new ListItemClickListner());
+    }
+
+    private class ListItemClickListner implements AdapterView.OnItemClickListener{
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id){
+            Map<String, String> item = (Map<String, String>) parent.getItemAtPosition(position);
+            String menuName = item.get("name");
+            String menuPrice = item.get("price");
+
+            Intent intent = new Intent(MenuListActivity.this, MenuThanksActivity.class);
+            intent.putExtra("menuName", menuName);
+            intent.putExtra("menuPrice", menuPrice);
+
+            startActivity(intent);
+        }
     }
 }
